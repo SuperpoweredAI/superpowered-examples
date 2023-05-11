3da6db192710b622611a0686eb98b6ecb4a2fa3f
Zach <zacharymccormick755@gmail.com>
2023-05-11 10:32:14 -0600

    added personal-kb-chrome-extension example

---
+++ personal-kb-chrome-extension/background.js
@@ -0,0 +1,41 @@
+// Replace these with your own API key ID and secret, and Knowledge Base ID
+const apiKeyId = 'YOUR_API_KEY_ID';
+const apiKeySecret = 'YOUR_API_KEY_SECRET';
+const kbId = 'YOUR_KNOWLEDGE_BASE_ID';
+
+const baseUrl = 'https://api.superpowered.ai/v1/';
+
+function init() {
+    const token = btoa(`${apiKeyId}:${apiKeySecret}`);
+    return `Bearer ${token}`;
+  }
+  
+const headers = {
+'Authorization': init(),
+'Content-Type': 'application/json'
+};
+
+function extractTextFromPage(data) {
+const {title, url, content} = data;
+
+const payload = {
+    title,
+    link_to_source: url,
+    content
+};
+
+fetch(`${baseUrl}knowledge_bases/${kbId}/documents/raw_text`, {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(payload)
+})
+.then(response => response.json())
+.then(data => console.log('Document uploaded:', data))
+.catch(error => console.error('Error uploading document:', error));
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+if (request.action === 'extractText') {
+    extractTextFromPage(request.data);
+}
+});
