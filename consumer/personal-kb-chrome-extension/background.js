fe18a0dcabfa1c2778183603df083c9d7e9fd197
Zach <zacharymccormick755@gmail.com>
2023-10-12 11:37:41 -0600

    updated chrome-extension to use auto_context

--- consumer/personal-kb-chrome-extension/background.js
+++ consumer/personal-kb-chrome-extension/background.js
@@ -8,34 +8,35 @@ const baseUrl = 'https://api.superpowered.ai/v1/';
 function init() {
     const token = btoa(`${apiKeyId}:${apiKeySecret}`);
     return `Bearer ${token}`;
-  }
-  
+}
+
 const headers = {
-'Authorization': init(),
-'Content-Type': 'application/json'
+    'Authorization': init(),
+    'Content-Type': 'application/json'
 };
 
 function extractTextFromPage(data) {
-const {title, url, content} = data;
+    const { title, url, content } = data;
 
-const payload = {
-    title,
-    link_to_source: url,
-    content
-};
+    const payload = {
+        title: title,
+        link_to_source: url,
+        content: content,
+        auto_context: true,
+    };
 
-fetch(`${baseUrl}knowledge_bases/${kbId}/documents/raw_text`, {
-    method: 'POST',
-    headers: headers,
-    body: JSON.stringify(payload)
-})
-.then(response => response.json())
-.then(data => console.log('Document uploaded:', data))
-.catch(error => console.error('Error uploading document:', error));
+    fetch(`${baseUrl}knowledge_bases/${kbId}/documents/raw_text`, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(payload)
+    })
+        .then(response => response.json())
+        .then(data => console.log('Document uploaded:', data))
+        .catch(error => console.error('Error uploading document:', error));
 }
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-if (request.action === 'extractText') {
-    extractTextFromPage(request.data);
-}
+    if (request.action === 'extractText') {
+        extractTextFromPage(request.data);
+    }
 });
